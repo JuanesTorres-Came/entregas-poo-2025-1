21ef132acf3384295ebc0e2885c18474f5752825
JuanesTorres <storresjuane.10@gmail.com>
2025-03-29 22:57:42 -0500

    update

---
+++ Mascotas1/Mascotas1.py
@@ -0,0 +1,70 @@
+"""
+Título de práctica: Mascotas 1
+
+Autor: Juan Torres <jetorress@academia.usbbog.edu.co>
+Fecha: 07/03/2025
+"""
+import datetime
+class Mascota:
+    def __init__(self, clase, nombre, edad, raza, fecha_ingreso):
+        self.clase = clase
+        self.nombre = nombre
+        self.edad = edad
+        self.raza = raza
+        self.fecha_ingreso = fecha_ingreso
+        
+    def obtener_datos (self):
+        #organiza los datos de Mascota en forma de lista
+        return [
+                f"{self.clase}",
+                f"{self.nombre}",
+                f"{self.edad}",
+                f"{self.raza}",
+                f"{self.fecha_ingreso.strftime('%Y-%m-%d')}"]
+
+def ingresar_mascotas():
+        #los datos que ingresara el cliente
+        clase = input ("ingrese la clase de la mascota: ")
+        nombre = input("Ingrese el nombre de la mascota: ")
+        edad = int(input(f"La edad de {nombre} es: "))
+        raza = input(f"la raza de {nombre} es: ")
+        fecha_ingreso = datetime.date.today()
+    
+        return Mascota(clase, nombre, edad, raza, fecha_ingreso)
+    
+def mostrar_mascotas(mascotas):
+        #encabezado de la tabla ("titulos")
+    encabezado = ["clase","nombre","edad","raza", "fecha_ingreso"]
+    print ("Mascotas ingresadas con sus datos:")
+    print (
+        #se muestran los productos en forma de tabla
+        f"{encabezado[0]:<20} {encabezado[1]:<20} {encabezado[2]:15}"
+        f"{encabezado[3]:<20} {encabezado[4]:<20}")
+            
+        
+    for mascota in mascotas:
+        (
+        clase, nombre, edad, raza, fecha_ingreso
+        )= mascota.obtener_datos()
+        print(
+            f"{clase:<20}" f"{nombre:<20}" f"{edad:<15}" 
+            f"{raza:<20}"  f"{fecha_ingreso:<20}"
+                
+            )
+def main():
+    nmascotas = int(input("cuantas mascotas va a ingresar?: "))
+    # Se pide el numero de mascotas que quiera ingresar el usuario
+    mascotas = []
+    # Se pide los datos para n mascotas
+    for i in range(nmascotas):
+        print(f"mascota {i+1}:")
+        mascota= ingresar_mascotas()
+        mascotas.append(mascota)
+    # Se muestran los productos que se ingresaron en formato de tabla
+    mostrar_mascotas(mascotas)
+    
+    
+                
+if __name__ == "__main__":
+    main()
+        
